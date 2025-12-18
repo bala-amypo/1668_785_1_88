b8abc316b34276157da4f8e9fa290192d840b8c4
bala-amypo <bala@amypo.in>
2025-12-18 18:01:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/BookingLogRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.repository;
+
+@Repository
+public interface BookingLogRepository extends JpaRepository <BookingLogRepository,Long>{
+
+}
---
+++ src/main/java/com/example/demo/repository/FacilityRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.repository;
+
+@Repository
+public interface FacilitytRepository extends JpaRepository <Facility,Long>{
+
+}
