9f4313483a5418e182811021b266f85e140938ab
bala-amypo <bala@amypo.in>
2025-12-20 12:42:30 +0000

    User commit

--- src/main/java/com/example/demo/model/ApartmentUnitModel.java
+++ src/main/java/com/example/demo/model/ApartmentUnitModel.java
@@ -3,5 +3,17 @@ package com.example.demo;
 import java.util.Date;
 import jakarta.persistence.Model;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Generated;
-import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SApartmentUnitModel{
+    @Id
+    @GeneratedValue(strategy=GenerationType)
+}
