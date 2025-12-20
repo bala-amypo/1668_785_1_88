38ce309d5019d07f51a41db9902b718178287f02
bala-amypo <bala@amypo.in>
2025-12-20 12:39:30 +0000

    User commit

--- src/main/java/com/example/demo/model/ApartmentUnitModel.java
+++ src/main/java/com/example/demo/model/ApartmentUnitModel.java
@@ -1,1 +1,7 @@
 package com.example.demo;
+
+import java.util.Date;
+import jakarta.persistence.Model;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Generated;
+import jakarta.persistence.Entity;
