1c6e97573141702a5831bf771c75fe360c0176b9
bala-amypo <bala@amypo.in>
2025-12-21 12:54:30 +0000

    User commit

--- src/main/java/com/example/demo/service/FacilityService.java
+++ src/main/java/com/example/demo/service/FacilityService.java
@@ -7,6 +7,6 @@ public interface FacilityService{
     Facility postData2(Facility use);
     List<Facility>getAllData2();
     String  DeleteData2(Long id);
-    User getData2(Long id);         
-    User updateData2(Long id,Facility entity);                                                        
+    Facility getData2(Long id);         
+    Facility updateData2(Long id,Facility entity);                                                        
 }
