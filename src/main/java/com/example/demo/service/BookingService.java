c159580a4c561ccc7e0d29dd25d514615c16eab6
bala-amypo <bala@amypo.in>
2025-12-21 13:15:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/BookingService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import java.util.List;
+import com.example.demo.model.Booking;
+
+public interface BookingService{
+    Booking postData3(Booking use);
+    List<Booking>getAllData3();
+    String  DeleteData3(Long id);
+    Booking getData3(Long id);         
+    Booking updateData3(Long id,Booking entity);                                                        
+}
---
+++ src/main/java/com/example/demo/service/Impl/BookingServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;   
+import com.example.demo.model.Facility;
+import com.example.demo.repository.FacilityRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.service.FacilityService;                
+
+@Service
+public class FacilityServiceImpl implements FacilityService{
+
+    @Autowired FacilityRepository used;
+    @Override
+    public Facility postData2(Facility use){
+        return used.save(use);  
+    }
+   
+    @Override
+    public List<Facility>getAllData2(){
+        return used.findAll();
+    }
+    @Override
+    public String DeleteData2(Long id){
+        used.deleteById(id);
+        return "Deleted successfully";
+    }
+    @Override
+    public Facility getData2(Long id){
+    return used.findById(id).orElse(null);
+    }
+    @Override
+    public Facility updateData2(Long id,Facility entity){
+        if(used.existsById(id)){
+            entity.setId(id);
+            return used.save(entity);
+        } 
+        return null;
+    }
+}
