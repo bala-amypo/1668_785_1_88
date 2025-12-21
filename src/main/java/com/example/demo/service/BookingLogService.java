dfc95ab475b81a6b59400964e40dc54216e7f303
bala-amypo <bala@amypo.in>
2025-12-21 13:27:30 +0000

    User commit

--- src/main/java/com/example/demo/service/BookingLogService.java
+++ src/main/java/com/example/demo/service/BookingLogService.java
@@ -1,12 +1,12 @@
 package com.example.demo.service;
 
 import java.util.List;
-import com.example.demo.model.Booking;
+import com.example.demo.model.BookingLog;
 
-public interface BookingService{
-    Booking postData3(Booking use);
-    List<Booking>getAllData3();
-    String  DeleteData3(Long id);
-    Booking getData3(Long id);         
-    Booking updateData3(Long id,Booking entity);                                                        
+public interface BookingLogService{
+    BookingLog postData4(BookingLog use);
+    List<BookingLog>getAllData4();
+    String  DeleteData4(Long id);
+    BookingLog getData4(Long id);         
+    BookingLog updateData4(Long id,BookingLog entity);                                                        
 }
---
+++ src/main/java/com/example/demo/service/Impl/BookingLogServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;   
+import com.example.demo.model.BookingLog;
+import com.example.demo.repository.BookingLogRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.service.BookingLogService;                
+
+@Service
+public class BookingLogServiceImpl implements BookingLogService{
+
+    @Autowired BookingLogRepository used;
+    @Override
+    public BookingLog postData4(BookingLog use){
+        return used.save(use);  
+    }
+   
+    @Override
+    public List<BookingLog>getAllData4(){
+        return used.findAll();
+    }
+    @Override
+    public String DeleteData3(Long id){
+        used.deleteById(id);
+        return "Deleted successfully";
+    }
+    @Override
+    public Booking getData3(Long id){
+    return used.findById(id).orElse(null);
+    }
+    @Override
+    public Booking updateData3(Long id,Booking entity){
+        if(used.existsById(id)){
+            entity.setId(id);
+            return used.save(entity);
+        } 
+        return null;
+    }
+}
