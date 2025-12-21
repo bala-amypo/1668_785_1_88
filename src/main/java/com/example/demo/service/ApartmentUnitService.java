5aa87bbb30420a0dc284fb7b6e9f07e6b0725302
bala-amypo <bala@amypo.in>
2025-12-21 13:37:36 +0000

    User commit

--- src/main/java/com/example/demo/service/ApartmentUnitService.java
+++ src/main/java/com/example/demo/service/ApartmentUnitService.java
@@ -3,10 +3,10 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.model.ApartmentUnit;
 
-public interface BookingLogService{
-    BookingLog postData4(BookingLog use);
-    List<BookingLog>getAllData4();
-    String  DeleteData4(Long id);
-    BookingLog getData4(Long id);         
-    BookingLog updateData4(Long id,BookingLog entity);                                                        
+public interface ApartmentUnitService{
+    ApartmentUnit postData5(ApartmentUnit use);
+    List<ApartmentUnit>getAllData5();
+    String  DeleteData5(Long id);
+    ApartmentUnit getData5(Long id);         
+    ApartmentUnit updateData5(Long id,ApartmentUnit entity);                                                        
 }
---
+++ src/main/java/com/example/demo/service/Impl/ApartmentUnitServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;   
+import com.example.demo.model.ApartmentUnit;
+import com.example.demo.repository.ApartmentUnitRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.service.ApartmentUnitService;                
+
+@Service
+public class ApartmentUnitgServiceImpl implements ApartmentUnitService{
+
+    @Autowired ApartmentUnitRepository used;
+    @Override
+    public ApartmentUnit postData5(ApartmentUnit use){
+        return used.save(use);  
+    }
+   
+    @Override
+    public List<ApartmentUnit>getAllData5(){
+        return used.findAll();
+    }
+    @Override
+    public String DeleteData5(Long id){
+        used.deleteById(id);
+        return "Deleted successfully";
+    }
+    @Override
+    public ApartmentUnit getData5(Long id){
+    return used.findById(id).orElse(null);
+    }
+    @Override
+    public ApartmentUnit updateData5(Long id,ApartmentUnit entity){
+        if(used.existsById(id)){
+            entity.setId(id);
+            return used.save(entity);
+        } 
+        return null;
+    }
+}
