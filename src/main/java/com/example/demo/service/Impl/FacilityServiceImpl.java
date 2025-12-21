2875d5f3e9075fa2b9245f92ed4ab7a7a1047bbe
bala-amypo <bala@amypo.in>
2025-12-21 12:28:03 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/FacilityServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;   
+import com.example.demo.model.Facility;
+import com.example.demo.repository.FacilityRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.service.FacilityService;                
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired UserRepository used;
+    @Override
+    public User postData1(User use){
+        return used.save(use);  
+    }
+    @Override
+    public User postdata(User log){
+        return used.save(log);
+    }
+    @Override
+    public List<User>getAllData1(){
+        return used.findAll();
+    }
+    @Override
+    public String DeleteData1(Long id){
+        used.deleteById(id);
+        return "Deleted successfully";
+    }
+    @Override
+    public User getData1(Long id){
+    return used.findById(id).orElse(null);
+    }
+    @Override
+    public User updateData1(Long id,User entity){
+        if(used.existsById(id)){
+            entity.setId(id);
+            return used.save(entity);
+        } 
+        return null;
+    }
+}
