5838e6a6158eca292d60d489a3fdc9e325825ff8
bala-amypo <bala@amypo.in>
2025-12-26 12:41:05 +0000

    User commit

--- src/main/java/com/example/demo/model/Booking.java
+++ src/main/java/com/example/demo/model/Booking.java
@@ -38,8 +38,6 @@
 
 
 
-
-
 package com.example.demo.model;
 
 import jakarta.persistence.*;
@@ -59,8 +57,13 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String facilityName;
+    @ManyToOne
+    @JoinColumn(name="facility_id", nullable=false)
+    private Facility facility;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable=false)
+    private User user;
 
     @Column(nullable = false)
     private LocalDateTime startTime;
@@ -69,15 +72,15 @@ public class Booking {
     private LocalDateTime endTime;
 
     @Column(nullable = false)
-    private String bookedBy;
-
-    @Column(nullable = false)
     private String status;
+
+    public static final String STATUS_CONFIRMED = "CONFIRMED";
+    public static final String STATUS_CANCELLED = "CANCELLED";
 
     @PrePersist
     public void onCreate() {
         if (this.status == null) {
-            this.status = "CONFIRMED";
+            this.status = STATUS_CONFIRMED;
         }
     }
 }
