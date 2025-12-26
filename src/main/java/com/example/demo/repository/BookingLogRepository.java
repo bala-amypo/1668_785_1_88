fbaa61e533cb5d8cf5fe2a173384f2b4dbc23e68
bala-amypo <bala@amypo.in>
2025-12-26 12:44:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/BookingLogRepository.java
+++ src/main/java/com/example/demo/repository/BookingLogRepository.java
@@ -13,14 +13,16 @@
 
 
 
-
-
 package com.example.demo.repository;
 
-import com.example.demo.model.*;
+import com.example.demo.model.Booking;
+import com.example.demo.model.BookingLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-public interface BookingLogRepository {
-    BookingLog save(BookingLog log);
+@Repository
+public interface BookingLogRepository extends JpaRepository<BookingLog, Long> {
     List<BookingLog> findByBookingOrderByLoggedAtAsc(Booking booking);
 }
--- src/main/java/com/example/demo/repository/FacilityRepository.java
+++ src/main/java/com/example/demo/repository/FacilityRepository.java
@@ -13,15 +13,15 @@
 
 
 
-
 package com.example.demo.repository;
 
 import com.example.demo.model.Facility;
-import java.util.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface FacilityRepository {
-    Optional<Facility> findById(Long id);
+import java.util.Optional;
+
+@Repository
+public interface FacilityRepository extends JpaRepository<Facility, Long> {
     Optional<Facility> findByName(String name);
-    Facility save(Facility facility);
-    List<Facility> findAll();
 }
