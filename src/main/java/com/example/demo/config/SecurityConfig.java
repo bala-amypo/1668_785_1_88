f298b1474cad0ee4fe9f323729774e3048597db7
bala-amypo <bala@amypo.in>
2025-12-28 17:42:27 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -25,7 +25,6 @@ public class SecurityConfig {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
-    // ✅ THIS FIXES YOUR ERROR
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
